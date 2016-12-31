63a2197ea67a70c2b07f6bd9cf21e14165de442a
Hey-Scotty <Scotty@Tevins-MacBook-Pro.local>
2016-12-31 18:17:19 -0500

    opponent sprite added

--- iOS Boxing/GameViewController.swift
+++ iOS Boxing/GameViewController.swift
@@ -12,26 +12,32 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+        if let scene = SKScene(fileNamed: "GameScene"){
+            // Configure the view.
+            let skView = self.view as! SKView
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            skView.showsPhysics = true
+            skView.showsDrawCount = true
+            
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = true
+            
+            /* Set the scale mode to scale to fit the window */
+            
+            if(skView.scene == nil){
                 
-                // Present the scene
-                view.presentScene(scene)
+                scene.scaleMode = .aspectFill
+                scene.size  = skView.bounds.size
+                skView.presentScene(scene)
             }
             
-            view.ignoresSiblingOrder = true
             
-            view.showsFPS = true
-            view.showsNodeCount = true
         }
     }
-
     override var shouldAutorotate: Bool {
         return true
     }
---
+++ iOS Boxing/Glove.swift
@@ -0,0 +1,12 @@
+//
+//  Glove.swift
+//  iOS Boxing
+//
+//  Created by Tevin Scott on 12/31/16.
+//  Copyright © 2016 Tevin Scott. All rights reserved.
+//
+
+import Foundation
+class Glove{
+    
+}
